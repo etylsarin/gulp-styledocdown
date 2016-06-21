08bb120f4e9598680fad6b7eb9f1bbabea44672f
Filip Mares <maresf@gmail.com>
2016-06-22 00:29:53 +0200

    gulp plugin syntax cleanup

--- index.js
+++ index.js
@@ -4,37 +4,44 @@ var path = require('path'),
 	oAssign = require('object-assign'),
 	sdd = require('styledocdown');
 
+var PLUGIN_NAME = 'gulp-styledocdown';
+
 module.exports = function (options) {
 	return through.obj(function (file, enc, cb) {
-		var cfg = {
-			root: path.dirname(file.path),
-			fileName: path.basename(file.path)
-		};
-
-		if (file.isNull()) {
-			cb(null, file);
-			return;
-		}
-
-		if (file.isStream()) {
-			cb(new gutil.PluginError('gulp-styledocdown', 'Streaming not supported'));
-			return;
-		}
+		var self = this,
+			cfg = {
+				root: path.dirname(file.path),
+				fileName: path.basename(file.path)
+			};
 
 		if (typeof options === 'object') {
 			cfg = oAssign(cfg, options);
 		}
 
+		if (file.isStream()) {
+			this.emit('error', new gutil.PluginError(PLUGIN_NAME, 'Streams are not supported!'));
+			return cb();
+		}
+
+		if (file.isNull()) {
+			return cb();
+		}
+
 		sdd(file.contents.toString(), cfg, function (err, data) {
 			if (err) {
-				cb(new gutil.PluginError('gulp-styledocdown', err, {fileName: file.path}));
-				return;
+				self.emit('error', new gutil.PluginError(PLUGIN_NAME, err, {fileName: file.path}));
+				return cb();
 			}
 
-			file.contents = new Buffer(data);
-			file.path += '.md';
+			if (data) {
+				if (file.isBuffer()) {
+					file.contents = new Buffer(data);
+					file.path += '.md';
+				}
+				self.push(file);
+			}
 
-			cb(null, file);
+			cb();
 		});
 	});
 };
